7efcb704f1cce930b268414fc4d96fe39bdb0164
Krzysztof Trojan <kris.trojan85@gmail.com>
2020-12-28 16:37:06 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const firstName = 'Krzysztof';
+const age = 35;
+const ona = 'Kasia';
+const dog1 = 'Aspen';
+const dog2 = 'Viki';
+const cat = 'Zander';
+
+
+console.log(firstName)
+console.log(age)
+
+console.log(`Siema! Nazywam sie ${firstName} i mam ${age} lat.`);
+console.log(`Ja ${firstName}, mam dosc przewalone, bo ${ona} przyprowadzila do domu ${cat} i ${dog2} zeby ${dog1} mial kolezkow ;)`);
