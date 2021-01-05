4c37d6096fac3a8d115708090cd94aaf186aac08
Krzysztof Trojan <kris.trojan85@gmail.com>
2021-01-04 22:39:23 -0600

    css modify

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-const firstName = 'Krzysztof';
+const firstName = "Krzysztof";
 const age = 35;
-const ona = 'Kasia';
-const dog1 = 'Aspen';
-const dog2 = 'Viki';
-const cat = 'Zander';
+const ona = "Kasia";
+const dog1 = "Aspen";
+const dog2 = "Viki";
+const cat = "Zander";
 
-
-console.log(firstName)
-console.log(age)
+console.log(firstName);
+console.log(age);
 
 console.log(`Siema! Nazywam sie ${firstName} i mam ${age} lat.`);
-console.log(`Ja ${firstName}, mam dosc przewalone, bo ${ona} przyprowadzila do domu ${cat} i ${dog2} zeby ${dog1} mial kolezkow ;)`);
+console.log(
+    `Ja ${firstName}, mam dosc przewalone, bo ${ona} przyprowadzila do domu ${cat} i ${dog2} zeby ${dog1} mial kolezkow ;)`
+);
